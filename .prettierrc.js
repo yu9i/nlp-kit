7e2ea35b745a9f258ad63b66613a1dd17f06f4cf
yu9i <yusun91317@gmail.com>
2024-09-20 11:11:41 +0900

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports - {
+    bracketSpacing: false,
+    jscBracketSameLine: true,
+    singleQuote: true,
+    trailingComma: 'none',
+    arrowParens: 'avoid',
+    semi: false,
+    printWidth: 90
+}
